e8358153d8f38a30a7fdb6561b2ef5e8bdf0290b
Rox <you@example.com>
2021-11-09 13:13:49 +0100

    new-file

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log('hola')
+const express = require('express')
+const app = express()
+
+
+app.use(express.static(__dirname + "/public"));
+
+
+app.get("/", (request, response) => {
+    response.sendFile(__dirname + "/vistas/home.html");
+})
+
+app.get("/about", (request, response) => {
+    response.sendFile(__dirname + "/vistas/about.html");
+})
+
+app.get("/works", (request, response) => {
+    response.sendFile(__dirname + "/vistas/works.html");
+})
+
+app.get("/photogallery", (request, response) => {
+    response.sendFile(__dirname + "/vistas/photogallery.html");
+})
+
+app.listen(3000)
